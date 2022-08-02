c5021cf5e9abed833b38b79e2ff7616bff68acb2
sivasankarkk <109886104+sivasankarkk@users.noreply.github.com>
2022-08-02 06:09:03 +0530

    Add files via upload

---
+++ components/index.js
@@ -0,0 +1,22 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard"
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
+import { RectButton, CircleButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from './DetailsDesc';
+
+export {
+  HomeHeader,
+  NFTCard,
+  SubInfo,
+  People,
+  EndDate,
+  FocusedStatusBar,
+  RectButton,
+  CircleButton,
+  EthPrice,
+  NFTTitle,
+  DetailsBid,
+  DetailsDesc,
+}
